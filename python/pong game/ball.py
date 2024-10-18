72a3aad7e26e1e2919773b865f98a2d1cdbdc8b4
DeenuRSD <124046429+DeenuRSD@users.noreply.github.com>
2024-10-18 15:23:10 +0530

    Update ball.py

--- python/pong game/ball.py
+++ python/pong game/ball.py
@@ -6,20 +6,24 @@ class Ball(Turtle):
         self.color("white")
         self.shape("circle")
         self.penup()
-        self.x_move=10
-        self.y_move=10
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x=self.xcor() + self.x_move
-        new_y=self.ycor() + self.y_move
-        self.goto(new_x,new_y)
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
 
     def bounce_y(self):
-        self.y_move*=-1
+        # Reverse the y direction
+        self.y_move *= -1
 
     def bounce_x(self):
-        self.x_move*=-1
+        # Reverse the x direction
+        self.x_move *= -1
 
     def reset_position(self):
-        self.goto(0,0)
+        # Move the ball back to the center of the screen
+        self.goto(0, 0)
+        # Reverse the x direction to simulate a serve
         self.bounce_x()
